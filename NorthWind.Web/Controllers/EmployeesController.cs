3e6e65aba17b36dc226bffc64b398126148bb470
Ramprit Sahani <Rampritsahani@outlook.in>
2017-05-13 22:30:47 +0530

    Add project files.

---
+++ NorthWind.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,121 @@
+﻿
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Mvc;
+using NorthwindDemo.Context;
+using NorthwindDemo.Entities;
+
+namespace NorthWind.Web.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private NorthwindDbContext db = new NorthwindDbContext();
+        private readonly GenericRepository<Employee> _repo;
+
+        public EmployeesController(GenericRepository<Employee> repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var employees = _repo.AllInclude(e => e.Employee_ReportsTo, e => e.Territories);
+            return View(employees);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = _repo.FindByKey(id.Value);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.ReportsTo = new SelectList(db.Employees, "EmployeeId", "LastName");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "EmployeeId,LastName,FirstName,Title,TitleOfCourtesy,BirthDate,HireDate,Address,City,Region,PostalCode,Country,HomePhone,Extension,Photo,Notes,ReportsTo,PhotoPath")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ReportsTo = new SelectList(db.Employees, "EmployeeId", "LastName", employee.ReportsTo);
+            return View(employee);
+        }
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ReportsTo = new SelectList(db.Employees, "EmployeeId", "LastName", employee.ReportsTo);
+            return View(employee);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "EmployeeId,LastName,FirstName,Title,TitleOfCourtesy,BirthDate,HireDate,Address,City,Region,PostalCode,Country,HomePhone,Extension,Photo,Notes,ReportsTo,PhotoPath")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ReportsTo = new SelectList(db.Employees, "EmployeeId", "LastName", employee.ReportsTo);
+            return View(employee);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = _repo.FindByKey(id.Value);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = _repo.FindByKey(id);
+            _repo.Delete(employee.EmployeeId);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
